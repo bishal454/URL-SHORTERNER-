0dcfd44553004f3ec50e7226f945bddfcb88e2ec
=BISHAL RANJAN SAH <bishalranjan44@gmail.com>
2025-06-28 18:52:12 +0545

    redirecting done and short_url wihout frontend and modularization

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ import {nanoid} from "nanoid"
 import dotenv from "dotenv"
 import connectDB from "./src/config/monogo.config.js"
 import  urlSchema from "./src/models/short_url.model.js"
+import shortUrl from "./src/models/short_url.model.js"
 
 
 dotenv.config("./.env");
@@ -23,7 +24,20 @@ app.post("/api/create",(req,res)=>{
     })
     newUrl.save()
     console.log(url);
-    res.send(nanoid(6));
+    res.send(nanoid(7));
+})
+
+app.get("/:id",async(req,res)=>{
+    const {id}=req.params
+    const url =await urlSchema.findOne({short_url:id})
+    if(url){
+        res.redirect(url.full_url)
+
+    }
+    else{
+        res.status(404).send("Not Found")
+
+    }
 })
 
 app.listen(3000,()=>{
