26ce41823c431aac428d67d6d1c402247b680ec0
=BISHAL RANJAN SAH <bishalranjan44@gmail.com>
2025-06-28 18:37:12 +0545

    created and saved the short_url now for redirection

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,13 @@
 import express from "express"
 import {nanoid} from "nanoid"
+import dotenv from "dotenv"
+import connectDB from "./src/config/monogo.config.js"
+import  urlSchema from "./src/models/short_url.model.js"
+
+
+dotenv.config("./.env");
+
+
 
 const app =express();
 
@@ -8,10 +16,17 @@ app.use(express.urlencoded({extended:true}))
 
 app.post("/api/create",(req,res)=>{
     const {url}=req.body
+    const shortUrl =nanoid(7)
+    const newUrl =new urlSchema({
+        full_url:url,
+        short_url:shortUrl
+    })
+    newUrl.save()
     console.log(url);
     res.send(nanoid(6));
 })
 
 app.listen(3000,()=>{
+    connectDB()
     console.log("server is running on http://localhost:3000");
 })
