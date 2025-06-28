86b0a40cf5f0ef1a0abadd322f5facfe8531d98b
=BISHAL RANJAN SAH <bishalranjan44@gmail.com>
2025-06-29 00:41:11 +0545

    tenstack setup done

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ import  urlSchema from "./src/models/short_url.model.js"
 import short_url from "./src/models/short_url.model.js"
 import  {redirectFromShortUrl} from "./src/controllers/short_url.controller.js"
 import {errorHandler} from "./src/utils/errorHandler.js"
+import cors from "cors"
 
 dotenv.config("./.env");
 
@@ -13,6 +14,7 @@ dotenv.config("./.env");
 
 const app =express();
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
